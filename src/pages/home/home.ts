f43bdb84cbbc32114f64e10b399346f4350e366c
Edgar <Edgaramador@ingenieros.com>
2018-04-30 23:08:25 -0500

    Manejo de ALert y Loading

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
+import { NavController, AlertController } from 'ionic-angular';
 
 import { Pagina2Page } from "../pagina2/pagina2";
 @Component({
@@ -11,8 +10,32 @@ export class HomePage {
 
   pagina2 = Pagina2Page;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
   }
+
+/*
+  showConfirm() {
+    let confirm = this.alertCtrl.create({
+      title: 'Use this lightsaber?',
+      message: 'Do you agree to use this lightsaber to do good across the intergalactic galaxy?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          handler: () => {
+            console.log('Fieled to navigate');
+          }
+        },
+        {
+          text: 'Aceptar',
+          handler: () => {
+            console.log('Agree clicked');
+            this.navCtrl.push(Pagina2Page);
+          }
+        }
+      ]
+    });
+    confirm.present();
+  }
+*/
 
 }
--- src/pages/pagina2/pagina2.ts
+++ src/pages/pagina2/pagina2.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, LoadingController, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-pagina2',
@@ -7,55 +7,93 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class Pagina2Page {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    public loadingCtrl: LoadingController,
+    private alertCtrl: AlertController) {
   }
 
-  ir_pagina3(){
+  ir_pagina3() {
     this.navCtrl.push("mi-pagina3");
   }
 
-  ionViewDidLoad(){
+  ionViewDidLoad() {
     console.log("ionViewDidLoad");
   }
 
-  ionViewWillEnter(){
+  ionViewWillEnter() {
     console.log("ionViewWillEnter");
   }
 
-  ionViewDidEnter(){
+  ionViewDidEnter() {
     console.log("ionViewDidEnter");
   }
 
-  ionViewWillLeave(){
+  ionViewWillLeave() {
     console.log("ionViewWillLeave");
   }
 
-  ionViewDidLeave(){
+  ionViewDidLeave() {
     console.log("ionViewDidLeave");
   }
 
-  ionViewWillUnload(){
+  ionViewWillUnload() {
     console.log("ionViewWillUnload");
   }
 
   //Controla el INGRESO
-  ionViewCanEnter(){ 
-    console.log("ionViewCanEnter");
-    let numero =  Math.round(Math.random() * 10);
+  ionViewCanEnter() {
+
+    let promesa = new Promise((resolve, reject) => {
+      let confirm = this.alertCtrl.create({
+        title: '¿Seguro?',
+        message: 'Esta seguro desea entrar?',
+        buttons: [
+          {
+            text: 'Cancelar',
+            handler: () => resolve(false)
+          },
+          {
+            text: 'Aceptar',
+            handler: () => resolve(true)
+          }
+        ]
+      });
+      confirm.present();
+    });
+
+    return promesa;
+    /*console.log("ionViewCanEnter");
+    let numero = Math.round(Math.random() * 10);
     console.log(numero);
-    if(numero >= 3)
-    { return true} else {return false};
+    if (numero >= 3) { return true } else { return false };*/
   }
 
   //Controla la SALIDA
-  ionViewCanLeave(){
+  ionViewCanLeave() {
     console.log("ionViewCanLeave");
     console.log("Espera 2 segundos para salir");
-    let promesa = new Promise((resolve, reject) =>{
-      setTimeout(()=>{
+
+    //Loading
+    let loader = this.loadingCtrl.create({
+      content: "Espere por favor..."
+    });
+    loader.present();
+
+    let promesa = new Promise((resolve, reject) => {
+      setTimeout(() => {
         resolve(true)
-      }, 2000)
+
+        loader.dismiss();
+      }, 2000);
     });
     return promesa;
+  }
+
+  presentLoading() {
+    content: "Espere por favor..."
+    let loader = this.loadingCtrl.create({
+    });
+    loader.present();
   }
 }
